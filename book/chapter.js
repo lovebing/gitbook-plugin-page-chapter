5b51245f8ff78cb3cc53b9643359f4849de98f6a
lovebing <tangyangjian@gmail.com>
2021-06-09 17:44:05 +0800

    improve

--- book/chapter.js
+++ book/chapter.js
@@ -17,12 +17,27 @@ require(['gitbook', 'jQuery'], function (gitbook, $) {
 		chapter.html('');
 		var nodes = document.querySelector('.page-inner')
 		.querySelectorAll('h1[id], h2[id], h3[id], h4[id], h5[id], h6[id]');
+		var items = [];
 		$.each(nodes, function(k, v) {
 			var item = $('<div class="item item-' + v.tagName.toLowerCase() + '" />');
 			var a = $('<a href="#' + v.id + '" />');
 			a.html(v.innerText);
 			item.append(a);
 			chapter.append(item);
+			items.push(item);
+		});
+
+		$('.body-inner').unbind('scroll').bind('scroll', function (e) {
+			for (var i = 0; i < nodes.length; i++) {
+				var v = nodes[i].getBoundingClientRect().top;
+				if (v >= 0 && v <= 10) {
+					$.each(items, function (k2, item) {
+						item.removeClass('active');
+					});
+					items[i].addClass('active');
+					break;
+				}
+			}
 		});
   	});
 });
